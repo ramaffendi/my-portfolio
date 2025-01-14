fa4b0c2ebee8fd73fe8a6cfd1d735b11fe0c7461
gagalName <ramadhaneffendi98@gmail.com>
2025-01-15 06:12:21 +0700

    cv-ku

--- src/components/Skills/Index.jsx
+++ src/components/Skills/Index.jsx
@@ -123,8 +123,7 @@ const Skills = () => {
       <Wrapper>
         <Title>Skills</Title>
         <Desc>
-          Here are some of my skills on which I have been working on for the
-          past 2 years.
+          Here are some of my skills that I have learned this year and last.
         </Desc>
         <SkillsContainer>
           {skills.map((skill) => (
